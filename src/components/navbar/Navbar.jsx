96e5b72b2436dfec8698495abe9c1ff8874639f7
arturhonores <61164887+arturhonores@users.noreply.github.com>
2024-01-08 20:05:38 +0000

    second commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/*Sidebar*/}
+      Navbar
+      <div className="wrapper"></div>
+    </div>
+  );
+};
+
+export default Navbar;
