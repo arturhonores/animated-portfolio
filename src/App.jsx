f720dfc4a8409bedf2c5ce4e14cd4db0006344f2
arturhonores <61164887+arturhonores@users.noreply.github.com>
2024-01-11 20:52:47 +0000

    framer-learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Test from "./Test";
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
 
@@ -14,6 +15,7 @@ const App = () => {
       <section>Portfolio2</section>
       <section>Portfolio3</section>
       <section>Contact</section>
+      {/* <Test /> */}
     </div>
   );
 };
