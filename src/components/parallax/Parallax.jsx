f7d23d282a7dc5fc3ad207c417c839260463658e
arturhonores <61164887+arturhonores@users.noreply.github.com>
2024-01-23 20:57:28 +0000

    parallax

---
+++ src/components/parallax/Parallax.jsx
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+import "./parallax.scss";
+import { motion, usesScroll, useTransform } from "framer-motion";
+
+const Parallax = ({ type }) => {
+  const ref = useRef();
+  const { scrollYProgress } = usesScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  return (
+    <div
+      className="parallax"
+      ref={ref}
+      style={{
+        background:
+          type === "services"
+            ? "linear-gradient(180deg, #111132, #0c0c1d)"
+            : "linear-gradient(180deg, #111132, #505064)",
+      }}
+    >
+      <motion.h1 style={{ y: yBg }}>
+        {type === "services" ? "What we do?" : "what we did?"}
+      </motion.h1>
+      <motion.div className="mountains"></motion.div>
+      <motion.div className="planets"></motion.div>
+      <motion.div className="stars"></motion.div>
+    </div>
+  );
+};
+
+export default Parallax;
