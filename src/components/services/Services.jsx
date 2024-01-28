99cec63ee8921c41fb832e08a0c55a68d3743ab3
arturhonores <luis.arturhonores@gmail.com>
2024-01-28 01:02:49 +0100

    parallax done

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,57 @@
+import "./services.scss"
+import { motion } from "framer-motion"
+
+const Services = () => {
+    return (
+        <motion.div className="services">
+            <motion.div className="textContainer">
+                <p>I focus on helping your brand grow <br />and move forward</p>
+            </motion.div>
+            <motion.div className="titleContainer">
+                <div className="title">
+                    <img src="/people.webp" alt="" />
+                    <h1>
+                        <b>Unique</b> Ideas
+                    </h1>
+                </div>
+                <div className="title">
+                    <h1>
+                        <b>For Your</b> Business.
+                    </h1>
+                    <button>WHAT WE DO?</button>
+                </div>
+            </motion.div>
+            <motion.div className="listContainer">
+                <div className="box">
+                    <h2>Branding</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo facere pariatur, ipsa quaerat sapiente rem harum repudiandae nihil magnam praesentium nam asperiores sed, soluta magni?
+                    </p>
+                    <button>Go</button>
+                </div>
+                <div className="box">
+                    <h2>Branding</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo facere pariatur, ipsa quaerat sapiente rem harum repudiandae nihil magnam praesentium nam asperiores sed, soluta magni?
+                    </p>
+                    <button>Go</button>
+                </div>
+                <div className="box">
+                    <h2>Branding</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo facere pariatur, ipsa quaerat sapiente rem harum repudiandae nihil magnam praesentium nam asperiores sed, soluta magni?
+                    </p>
+                    <button>Go</button>
+                </div>
+                <div className="box">
+                    <h2>Branding</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo facere pariatur, ipsa quaerat sapiente rem harum repudiandae nihil magnam praesentium nam asperiores sed, soluta magni?
+                    </p>
+                    <button>Go</button>
+                </div>
+            </motion.div>
+        </motion.div>
+    )
+}
+export default Services
