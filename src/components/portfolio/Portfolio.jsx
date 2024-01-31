a360cff36d8ed48e7da6792744c0b2ecd9eade5d
arturhonores <61164887+arturhonores@users.noreply.github.com>
2024-01-31 21:00:59 +0000

    portfolio2

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import "./portfolio.scss";
 import { useRef } from "react";
-import { motion, useScroll, useSpring } from "framer-motion";
+import { motion, px, useScroll, useSpring, useTransform } from "framer-motion";
 
 const items = [
   {
@@ -12,25 +12,46 @@ const items = [
   {
     id: 2,
     title: "Next Commerce",
-    img: "https://images.pexels.com/photos/109371/pexels-photo-109371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    img: "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, veniam fugit beatae voluptas, magni velit fuga deleniti nihil asperiores vero debitis distinctio quod.",
   },
   {
     id: 3,
     title: "Air Nostrum",
-    img: "https://images.pexels.com/photos/109371/pexels-photo-109371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    img: "https://images.pexels.com/photos/3987066/pexels-photo-3987066.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, veniam fugit beatae voluptas, magni velit fuga deleniti nihil asperiores vero debitis distinctio quod.",
   },
   {
     id: 4,
     title: "Moove Cars",
-    img: "https://images.pexels.com/photos/109371/pexels-photo-109371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    img: "https://images.pexels.com/photos/1188751/pexels-photo-1188751.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, veniam fugit beatae voluptas, magni velit fuga deleniti nihil asperiores vero debitis distinctio quod.",
   },
 ];
 
 const Single = ({ item }) => {
-  return <section>{item.title}</section>;
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const y = useTransform(scrollYProgress, [0, 1], ["0%", "-300%"]);
+  return (
+    <section ref={ref}>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer">
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>View work</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 const Portfolio = () => {
   const ref = useRef();
