fcf6d4541bfc3de54861c76a92e0554c921a85ad
arturhonores <61164887+arturhonores@users.noreply.github.com>
2024-01-31 21:32:31 +0000

    polyfill

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Polyfill para desplazamiento suave
+if ("scrollBehavior" in document.documentElement.style === false) {
+  (async () => {
+    const smoothscroll = await import("smoothscroll-polyfill");
+    smoothscroll.polyfill();
+  })();
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
