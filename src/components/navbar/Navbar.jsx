fba55587488816276d86273394871f097d5af6b5
arturhonores <61164887+arturhonores@users.noreply.github.com>
2024-02-05 19:23:49 +0000

    repair1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,13 +7,13 @@ const Navbar = () => {
     <div className="navbar">
       <Sidebar />
       <div className="wrapper">
-        <motion.span
+        {/* <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1.5 }}
         >
           ArturHonores
-        </motion.span>
+        </motion.span> */}
         <div className="social">
           <a href="#">
             <img src="/facebook.png" alt="" />
